1693785670b6665b8566c6511014980c7fef37a6
Tim Anand <timanand@gmail.com>
2024-01-05 18:46:43 +0000

    Add user input for password length

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let cancelledByUser = false; 
+
+const numericRegex = /^[0-9]+$/;
+
+const msgEnterPwdLength  = "How many characters (8 to 128) would you like your password to contain ?"
+const msgPwdLengthValid = "It needs to be a numeric value at least 8 characters and up to 128 characters.";
+const msgPwdLengthValidError = "Length of Password entered is invalid.";
+const msgPwdLengthNonNumericValidError = "Invalid input for Password Length.";
+
+
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -88,8 +99,57 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+
+// Prompt user to enter length of password (8 to 128).
+// If user clicks on cancel then aborts prompt
+function promptLengthPwd() {
+
+  let pwdLength;
+  let invalidNo = true;
+
+  while (invalidNo) { 
+
+    let userInput = prompt(msgEnterPwdLength);
+
+    if (userInput === null) {
+      alert("Cancelled by user");
+      cancelledByUser=true;
+      break;
+    }
+
+    if (numericRegex.test(userInput)) {
+      
+      pwdLength = parseInt(userInput);
+      if (pwdLength >=8 && pwdLength <=128)
+      {
+        invalidNo = false;
+      }
+      else 
+      {
+        alert(msgPwdLengthValidError + ' ' + msgPwdLengthValid + " Please try again.");
+        continue;
+      }
+    }
+    else {
+      alert(msgPwdLengthNonNumericValidError + ' ' + msgPwdLengthValid);
+      continue;
+    }
+  }
+
+  return pwdLength;
+}
+
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+
+  // Prompt user for Length of Password
+  let pwdLength = promptLengthPwd();
+
+  // If user cancelled then exit function since no point continuing
+  if (cancelledByUser) {
+    return;
+  }
 
 }
 
@@ -100,7 +160,7 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  getPasswordOptions();
 }
 
 // Get references to the #generate element
